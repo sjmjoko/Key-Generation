592565c29ab250406ec59d98a53058059a72d2ec
sjmjoko <sjmjoko@gmail.com>
2016-05-17 02:42:53 +0200

    DES is complete. Try to fix that left thingie.

--- keyGeneration/key.cpp
+++ keyGeneration/key.cpp
@@ -7,6 +7,7 @@
 #include<array>
 #include<algorithm>
 #include"keyHeader.h"
+
 //PC-1 Table
 int pc_1_Table[] = { 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44,
 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4 };
@@ -17,47 +18,48 @@ int pc_2_Table[] = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 
 int IP_1[] = { 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
 57, 49, 41, 33, 25, 17, 9, 1, 9, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7 };
 //IP-2 Table
-int IP_2[] = { 40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36,
-4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25 };
+int IP_2[] = { 16 ,7 ,20, 21, 29, 12, 28, 17, 1 ,15, 23, 26, 5 ,18, 31, 10, 2, 8, 24, 14, 32, 32, 27, 3 ,9, 19, 13, 30, 6 ,22 ,11, 4,
+25 };
 //Expansion Table
 int expansion[] = { 32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1 };
 //S-BOX ARRAYS
+//S1 s-box table
 int S1[][16] = { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
 { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
 { 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
 { 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } };
-
+//S2 s-box table
 int S2[][16] = { { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
 { 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
 { 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
 { 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } };
-
+//S3 s-box table
 int S3[][16] = { { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 },
 { 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
 { 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
 { 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } };
-
+//S4 s-box table
 int S4[][16] = { { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
 { 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
 { 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
 { 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 } };
-
+//S5 s-box table
 int S5[][16] = { { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
 { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
 { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
 { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } };
-
+//S6 s-box table
 int S6[][16] = { { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
 { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
 { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
 { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } };
-
+//S7 s-box table
 int S7[][16] = { { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
 { 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
 { 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
 { 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } };
-
+//S8 s-box table
 int S8[][16] = { { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
 { 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
 { 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
@@ -65,7 +67,7 @@ int S8[][16] = { { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
 
 using namespace std;
 //KEY GENERATION FUNCTION
-template < class T >
+template < class T >  //This function prints a 1D vector of any data type
 void print(vector<T> v)
 {
 	for (int i = 0; i < v.size(); i++)
@@ -74,7 +76,7 @@ void print(vector<T> v)
 	}
 	cout << endl;
 }
-template< class X >
+template< class X >   //This function prints a 2D vector of any data type
 void print2D(vector< vector<X>> x)
 {
 	for (int i = 0; i < x.size(); i++)
@@ -86,6 +88,7 @@ void print2D(vector< vector<X>> x)
 		cout << endl;
 	}
 }
+//convert the character to ascii codes 
 vector<int> toASCII(vector<int> &v, vector<char> &key)
 {
 	int ascii;
@@ -96,6 +99,7 @@ vector<int> toASCII(vector<int> &v, vector<char> &key)
 	}
 	return v;
 }
+//convert the ascii codes to 8 bits
 int* toBINARY(vector<int> &ascii, int temp[])
 {
 	int k = 7, z = 0;
@@ -113,7 +117,7 @@ int* toBINARY(vector<int> &ascii, int temp[])
 	}
 	return temp;
 }
-template<class Y>
+template<class Y>  //resize any 2D vector to chosen number of rows and columns
 void Resize(vector<vector<Y> > &v, int rows, int columns)
 {
 	v.resize(rows);
@@ -122,6 +126,7 @@ void Resize(vector<vector<Y> > &v, int rows, int columns)
 		v[i].resize(columns);   //allocate space for columns
 	}
 }
+//perform PC-1 permutation to the key
 vector<int> pc1(int kt[],vector<int> &pk)
 {
 	for (int i = 0; i < 56; i++)
@@ -130,6 +135,7 @@ vector<int> pc1(int kt[],vector<int> &pk)
 	}
 	return pk;
 }
+//rotate the key according to the number of shifts
 vector<vector<int> > rotationSchedule(vector<vector<int> > &tk, vector<int> &pc, vector<int> x)
 {
 	int  roundBit[] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
@@ -150,6 +156,7 @@ vector<vector<int> > rotationSchedule(vector<vector<int> > &tk, vector<int> &pc,
 	}
 	return tk;
 }
+//perform PC-2 permutation to the key
 vector<vector<int> > pc2(vector<vector<int> > &v, vector<vector<int> > &k, int a[])
 {
 	for (int i = 0; i < ROWS; i++)
@@ -163,6 +170,7 @@ vector<vector<int> > pc2(vector<vector<int> > &v, vector<vector<int> > &k, int a
 }
 
 //MESSAGE GENERATION FUNCTIONS
+//perform IP-1 permutation on the plain text
 vector<vector<int> > ip1_permutation(vector<vector<int>> &a, int c[]) //vector confused,vector non confused, ip table
 {
 	for (int i = 0; i< a.size(); i++)
@@ -172,6 +180,7 @@ vector<vector<int> > ip1_permutation(vector<vector<int>> &a, int c[]) //vector c
 		}
 	return a;
 }
+//deletes spaces in the plain text
 vector<char> deleteSpaces(vector<char> & v)
 {
 	for (int k = 0; k < v.size(); k++)
@@ -183,6 +192,7 @@ vector<char> deleteSpaces(vector<char> & v)
 	}
 	return v;
 }
+//adds filler characters in the plain text
 vector<char> addFillers(vector<char> & v)
 {
 	while (v.size() % 8 != 0)
@@ -191,6 +201,7 @@ vector<char> addFillers(vector<char> & v)
 	}
 	return v;
 }
+//converts each char to ASCII codes
 vector<vector < int> > toAscii(vector<vector <char> > &c, vector<vector < int> > &v, int rownum)
 {
 	int ascii;
@@ -204,6 +215,7 @@ vector<vector < int> > toAscii(vector<vector <char> > &c, vector<vector < int> >
 	}
 	return v;
 }
+//expand the 32 bit text to 48-bits
 vector<int> expansionTable(vector<int> &right, vector<int> &temp)
 {
 	for (int i = 0; i < 48; i++)
@@ -212,6 +224,7 @@ vector<int> expansionTable(vector<int> &right, vector<int> &temp)
 	}
 	return right;
 }
+//perform XOR of the key and the plain text
 vector<int> XOR(vector<vector<int> > &k, vector<int> &t, vector<int> &c, int rowkey) //pass the row of the key being used
 {
 	int a, b;
@@ -223,6 +236,7 @@ vector<int> XOR(vector<vector<int> > &k, vector<int> &t, vector<int> &c, int row
 	}
 	return c;
 }
+//converts each char to 8-bits
 vector<vector<int> > toBinary(vector<vector<int> > &ascii, vector<vector<int> > &bin, vector<int> &mes, int rows) //mes is a emp vector
 {
 	int b = 0, c = 7, e = 0;
@@ -255,6 +269,7 @@ vector<vector<int> > toBinary(vector<vector<int> > &ascii, vector<vector<int> > 
 }
 
 //ENCRYPTION FUNCTIONS
+//convert the 1D 48 bit text to 2D (8x6)
 vector<vector<int> > to2D(vector<vector<int> > &v, vector<int> a)
 {
 	int t=0;
@@ -269,16 +284,19 @@ vector<vector<int> > to2D(vector<vector<int> > &v, vector<int> a)
 	}
 	return v;
 }
+//get rows for the Sboxes
 int row(int a, int b)
 {
 	int c = 2 * a + b;
 	return c;
 }
+//get columns for the Sboxes
 int column(int a, int b, int c, int d)
 {
 	int e = 8 * a + 4 * b + c * 2 + d;
 	return e;
 }
+//perform the reduction of 48 bits to 32 bits
 void sBox(vector<vector<int> > &v, vector<int> &x)
 {
 	int d;
@@ -336,6 +354,7 @@ void sBox(vector<vector<int> > &v, vector<int> &x)
 		x.push_back(p[j]);
 	}
 }
+//perform permutation to the output of the Sbox
 vector<int> pBox(vector<int> a, vector<int> &b)
 {
 	for (int i = 0; i < a.size(); i++)
@@ -344,6 +363,7 @@ vector<int> pBox(vector<int> a, vector<int> &b)
 	}
 	return b;
 }
+//transfer contents of one vect to another
 vector<int> passover(vector<int> &x, vector<int> y)
 {
 	for (int i = 0; i < y.size(); i++)
@@ -352,45 +372,26 @@ vector<int> passover(vector<int> &x, vector<int> y)
 	}
 	return x;
 }
-vector<int> xor(vector<int> &a, vector<int> b, vector<int> c)
+//perform XOR to R(i+1) and Li
+vector<int> xor(vector<int> &a, vector<int> &b, vector<int> &c)
 {
 	int x ,y;
-	for (int i = 0; i <b.size(); i++)
+	for (int i = 0; i <32; i++)
 	{
 		x = b[i];
 		y = c[i];
-		c.push_back(x^y);
+		a.push_back(x^y);
 	}
-	return c;
+	return a;
 }
-//void encryption(vector<vector<int> > &text, vector<vector<int> > &key, vector<int> cipher, int textRows)  //study this function
-//{
-//	int cycle = 0; //determines the number of rows to be encrypted
-//	while (cycle < textRows)
-//	{
-//		int keyIterator = 0;
-//		vector<int> Left, Right, temp, tempL; //temp is usd to store things temporarily
-//		//separate the text into 32 bits, left and right
-//		for (int i = 0; i < 32; i++)  //select the row of the text
-//		{
-//			Left.push_back(text[cycle][i]);
-//			Right.push_back(text[cycle][i + 32]);  //push the right text as a temporary text 32 bit
-//		}
-//		tempL.insert(tempL.end(), Right.begin(), Right.end()); //R(i)=L(i+1)
-//		//this part should be repeated 16 times
-//		expansionTable(temp, Right); //temp is the temporary expanded right text
-//		Right.clear();//clear the right variable to store the new Right text
-//		XOR(key, temp, Right, cycle);
-//	}
-//}
 int main()
 {
 	int k = 7, z = 0, l = 0;
 	char t;
 	string  originalKey;
-	cout << "Enter the key: ";
+	cout << "Enter the key: ";  //prompt the user to enter the key
 	getline(cin, originalKey);
-	cout << "Enter the message: ";
+	cout << "Enter the message: ";   //prompt the user to enter the plain text
 	getline(cin, message);
 	//transfer key to vector
 	for (int i = 0; i < originalKey.length(); i++)
@@ -426,8 +427,8 @@ int main()
 	////conversion to PC-2 table
 	pc2(tempKey, encryptionKey, pc_2_Table);
 	/*print2D(encryptionKey);*/
+
 	//GENERATION OF THE PLAIN TEXT
-	cout << rows << endl;
 	//transfer the message to a 2D character vector
 	for (int i = 0; i < rows; i++)
 	{
@@ -449,41 +450,37 @@ int main()
 	cout << endl;
 	//generate the text to be encrypted
 	//THE ENCRYPTING STARTS HERE
-	//THE ENCRYPTING STARTS HERE
-	//THE ENCRYPTING STARTS HERE
 	while (cycle < rows)
 	{
-		for (int i = 0; i < 32; i++)
+		for (int i = 0; i < 32; i++) tempTextLeft.push_back(textBin[cycle][i]);  //transfer the left half of the text in a specific row of 64 bits
+		for (int i = 32; i < textBin[cycle].size(); i++) tempTextRight.push_back(textBin[cycle][i]);  //transfer the right half of the text in a specific row of 64 bits
+		
+		for (int q = 0; q < 16; q++)                       //perform the encryption schedule 16 times
 		{
-			tempTextLeft.push_back(textBin[cycle][i]);
-		}
-		for (int i = 32; i < textBin[cycle].size(); i++)
-		{
-			tempTextRight.push_back(textBin[cycle][i]);
-		}
-		for (int i = 0; i < 16; i++)
-		{
-			expansionTable(rightText, tempTextRight);
-			tempTextRight.clear();
-			XOR(encryptionKey, rightText, cipherText, i);
+			expansionTable(rightText, tempTextRight);      //retrieve the text and perform the expansion from 32 bits to 48 bits
+			XOR(encryptionKey, rightText, cipherText, q);  //perform XOR of the 48 bit text and key at each round q
+			tempTextRight.clear();                         //vectors are cleared to be re-used
 			rightText.clear();
-			to2D(temp, cipherText);
+			to2D(temp, cipherText);                        //convert the temporary cipher text to 2D
 			cipherText.clear();
-			sBox(temp, tempTextRight);
-			pBox(tempTextRight, rightText);
+			sBox(temp, tempTextRight);                     //reduce the cipher text from 48 bits to 32 bits using S-boxes
+			pBox(tempTextRight, rightText);                //perform permutation to the output of the S-Box
 			tempTextRight.clear();
-			xor(cipherText,tempTextLeft,rightText);
-			passover(cipher, cipherText);
+			xor(cipherText,tempTextLeft,rightText);        //peform XOR to Li and R(i+1)
+			rightText.clear();
 			tempTextLeft.clear();
-			passover(tempTextLeft, cipher);
-			passover(tempTextRight, cipher);
-			cipher.clear();
+			passover(tempTextLeft, cipherText);            //L(i+1) = Ri
+			passover(tempTextRight, cipherText);           //Ri is to be encrypted 16 times
+			cipherText.clear();
 		}
-		passover(cipher, tempTextRight);
-		tempTextRight.clear();
+		for (int i = 0; i < tempTextLeft.size(); i++){ CIPHER.push_back(tempTextLeft[i]); }     //push L16 to the cipher text
+		for (int i = 0; i < tempTextRight.size(); i++){ CIPHER.push_back(tempTextRight[i]); }   //push R16 to the cipher text
+		tempTextRight.clear();   //clear this vector as it will be used in until the entire text is encrypted
 		tempTextLeft.clear();
-		cycle++;
+		cycle++;                //increment to another row of text
 	}
+	cout << CIPHER.size() << endl;
+	print(CIPHER);
 	system("pause");
 	return 0;
 }
--- keyGeneration/keyHeader.h
+++ keyGeneration/keyHeader.h
@@ -17,7 +17,7 @@
 using namespace std;
 vector< vector<int> > encryptionKey, tempKey, textBin, textAscii, textMsg,temp;    //2D  encryption key,binary text message
 vector<char> key, msg;         //char key and message
-vector<int> asciiKey, PC_1_key, x, asciiMsg, msgTemp, ipMsg, tempTextRight, cipherText, rightText,
+vector<int> asciiKey, PC_1_key, x, asciiMsg, msgTemp, ipMsg, tempTextRight, cipherText, rightText,CIPHER,
 tempTextLeft,cipher;//convert the cipher int a 2D vector
 int kTemp[64];
 vector<vector<char> > textChar;
@@ -53,6 +53,5 @@ void sBox(vector<vector<int> > &v, vector<int> &x);
 int row(int a, int b);
 int column(int a, int b, int c, int d);
 vector<int> passover(vector<int> &x, vector<int> y);
-vector<int> xor(vector<int> &a, vector<int> b, vector<int> c);
-//void encryption(vector<vector<int> > &text, vector<vector<int> > &key, vector<int> cipher, int textRows);
+vector<int> xor(vector<int> &a, vector<int> &b, vector<int> &c);
 #endif
